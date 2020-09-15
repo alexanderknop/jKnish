ab51b15580e3c59226e78bafbe9854982ad50dca
alexanderknop <aaknop@gmail.com>
2020-09-14 21:18:50 -0700

    Extend scanner tests

--- test/org/github/alexanderknop/jknish/scanner/ScannerTest.java
+++ test/org/github/alexanderknop/jknish/scanner/ScannerTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class ScannerTest {
 
     @Test
-    void scanTokens() {
+    void testVar() {
         test("var x = 1",
                 List.of(VAR, IDENTIFIER, EQUAL, NUMBER, EOF),
                 "Was not able to scan 'var x = 1'");
@@ -25,6 +25,31 @@ class ScannerTest {
         test("var x = 1 // this is a one line comment",
                 List.of(VAR, IDENTIFIER, EQUAL, NUMBER, EOF),
                 "Comments shouldn't matter in 'var x = 1'");
+    }
+
+    @Test
+    void testIf() {
+        test("if (true) 1",
+                List.of(IF, LEFT_PAREN, TRUE, RIGHT_PAREN, NUMBER, EOF),
+                "Was not able to scan 'if (true) 1'");
+        test("if (true) 1 else 2",
+                List.of(IF, LEFT_PAREN, TRUE, RIGHT_PAREN, NUMBER, ELSE, NUMBER, EOF),
+                "Was not able to scan 'if (true) 1 else 2'");
+    }
+
+    @Test
+    void testWhile() {
+        test("while (true) 1",
+                List.of(WHILE, LEFT_PAREN, TRUE, RIGHT_PAREN, NUMBER, EOF),
+                "Was not able to scan 'while (true) 1'");
+    }
+
+    @Test
+    void testClass() {
+        test("class Test { test {1} }",
+                List.of(CLASS, IDENTIFIER, LEFT_BRACE, IDENTIFIER,
+                        LEFT_BRACE, NUMBER, RIGHT_BRACE, RIGHT_BRACE, EOF),
+                "Was not able to scan 'class Test { test {1} }'");
     }
 
     private void test(String source, List<TokenType> expected, String message) {
